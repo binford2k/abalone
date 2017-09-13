85bfa565d476a0218ee6fe8289c4eb14aea2c4b0
Ben Ford <ben.ford@puppetlabs.com>
2017-09-13 10:36:59 -0700

    Send systemd notifications properly
    There's a bug in `systemd-notify` such that the command sends the message and then exits. Since its lifetime is so short, systemd doesn't have time to do the housekeeping to figure out which service unit to associate it with. The mitigation is to just open the socket and send the message directly ourselves, since our process is long-lived.
    
    See https://github.com/systemd/systemd/issues/2737
    
    Fixes #16

--- lib/abalone/watchdog.rb
+++ lib/abalone/watchdog.rb
@@ -3,6 +3,11 @@ class Abalone::Watchdog
     raise 'Watchdog needs a port to be set' unless options.include? :port
     raise 'Watchdog needs a logger to be set' unless options.include? :logger
     logger = options[:logger]
+    pid    = Process.pid
+    socket = Socket.new(Socket::AF_UNIX, Socket::SOCK_DGRAM, 0).tap do |socket|
+      socket.connect(Socket.pack_sockaddr_un(ENV['NOTIFY_SOCKET']))
+      socket.close_on_exec = true
+    end
 
     Thread.new do
       require 'net/http'
@@ -17,7 +22,8 @@ class Abalone::Watchdog
           http.start
           http.request_get('/heartbeat/ping') do |res|
             logger.debug "Heartbeat response: #{res.read_body}"
-            system('systemd-notify WATCHDOG=1')
+            #system('systemd-notify WATCHDOG=1')
+            socket.write("WATCHDOG=1\nMAINPID=#{pid}")
           end
         rescue => e
           logger.warn 'Abalone service failed heartbeat check!'
